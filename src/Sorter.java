499813efe98b8f99ab76b4b10492cbe243873135
Nawaf Almalki <nkmalki77@gmail.com>
2023-02-12 16:01:41 +0300

    Selection sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,5 +1,26 @@
 public class Sorter {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++){
+            array[i] = (int) (Math.random() * 100);
+        }
+        selectionSort(array);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
