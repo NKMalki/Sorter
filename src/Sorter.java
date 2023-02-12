7cfd112aebf81d9fd64e817b3716b41bb4abc820
Ali13140 <119455185+Ali13140@users.noreply.github.com>
2023-02-12 16:03:35 +0300

    "Insertion sort"

--- src/Sorter.java
+++ src/Sorter.java
@@ -2,4 +2,17 @@ public class Sorter {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+    void insertionSort(int array[]) {
+        int size = array.length;
+
+        for (int step = 1; step < size; step++) {
+            int key = array[step];
+            int j = step - 1;
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                --j;
+            }
+            array[j + 1] = key;
+        }
+    }
 }
